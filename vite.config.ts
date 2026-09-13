8068eda2b905ebb779c8c3aae8f197578f53c630
Wellington Ameida <64033017+wellyington@users.noreply.github.com>
2026-09-13 15:57:49 +0100

    Fix deployment path and add fallbacks

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/cv/',
   plugins: [react()],
   resolve: {
     alias: {
